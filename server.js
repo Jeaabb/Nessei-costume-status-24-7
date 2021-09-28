12df136524804233f08137e2032855812e4e8261
Jeremy <62273120+jeremy776@users.noreply.github.com>
2021-09-28 11:18:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const server = express();
 
-server.all('/', (req, res)=>{
-    res.send("[K]")
-})
+server.all('/', (req, res) => {
+  res.send("[K]")
+});
+
 function keepAlive(){
-    server.listen(3000, ()=>{console.log("READY")});
-}
-module.exports = keepAlive
+  server.listen(3000, ()=>{console.log("READY")});
+};
+
+module.exports = keepAlive;
